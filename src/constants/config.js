eac20016b1fc71ea82508d34afab7e5517fc6125
maximilianoc <6249466asd>
2021-04-07 09:45:48 -0300

    hotfix

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,1 +1,2 @@
-export const URL_API = process.env.URL_API || 'http://localhost:3004/api/';
+//export const URL_API = process.env.URL_API || 'http://localhost:3004/api/';
+export const URL_API = 'https://usd-api.herokuapp.com/api/';
